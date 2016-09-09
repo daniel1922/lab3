7b0a8d16cfd1ae468fb81330be0357042cfb4bc3
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-08 22:28:34 -0400

    version1

---
+++ src/pair_display.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pair_display {
+
+	public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);	
+
+	//Welcome the user to the App	
+	
+	//Ask the user for an interger
+	
+	//Loop through the interget from the user
+	}
+
+}
