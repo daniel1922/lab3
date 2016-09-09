9020fb145b5b57465ba8b0fae91dc5dc4e3e75c1
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-08 22:32:06 -0400

    version2

--- src/pair_display.java
+++ src/pair_display.java
@@ -4,13 +4,16 @@ public class pair_display {
 
 	public static void main(String[] args) {
 	
-	Scanner input = new Scanner(System.in);	
+		Scanner input = new Scanner(System.in);
+		
+		String number;
+		String loop;
 
 	//Welcome the user to the App	
 	
 	//Ask the user for an interger
 	
-	//Loop through the interget from the user
+	//Loop through the interget from the usergi
 	}
 
 }
