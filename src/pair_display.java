0adb220690098731a27d3f5bac31e950f4e33675
Daniel Schneider <daniel.p.schneider@gmail.com>
2016-09-10 14:54:49 -0400

    version8 it works

--- src/pair_display.java
+++ src/pair_display.java
@@ -1,38 +1,44 @@
-import java.util.Scanner; //import scanner to view 
+import java.util.Scanner; 
 
 public class pair_display { //public class name
 
 	public static void main(String[] args) { //main method
-	
+		
 		Scanner input = new Scanner(System.in); //allows you to type input into scanner
 		
-		String number; //type of string we will be using
-		String loop; //type of string we will be using
+		String number;
+		String loop;
 
 	//Welcome the user to the App	
 		System.out.println("Learn your squares and cubes! \n"); //printing out the statement. The \n makes a new line
 		
 	//Ask the user for an integer
+		do{
 		System.out.println("Enter and interger: ");
-		number = input.nextLine(); //string
+		number = input.nextLine(); 
 		//changed the string to a number
-		int x = Integer.parseInt(number); //
+		int x = Integer.parseInt(number);
+		
 	
 	//Loop through the integer from the user
-		System.out.println("Number  " + "Squared  "  + "Cubed\n");
-		for( int i = 0; i <= x ; i++){
-			System.out.println( i + "       " + i*i + "         " +  i*i*i);
-
+		System.out.println("Number  " + "Squared  "  + "Cubed\n" + "====    " + "====     "+ "====");
+		
+	//Loop through the interget from the user
+			
+		for( int i = 1; i <= x ; i++){
+			
+			System.out.println( i + "        " + i*i + "         " +  i*i*i);
+		}
 //			System.out.println(number);
 			
 			System.out.println("Continue(yes/no): ");
 				loop = input.nextLine();
+		}				
 				while(loop.equalsIgnoreCase("yes"));{ 			
 		}
 		}
-		}
-	}
-		
+}
+	
  	
 		
 
